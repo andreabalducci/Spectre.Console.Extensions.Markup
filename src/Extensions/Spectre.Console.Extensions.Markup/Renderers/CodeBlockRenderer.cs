50d80614cbac8fe2238dc2e96a6c43773aa1ef54
Colin Wilmans <colin.wilmans@crwsolutions.nl>
2025-04-14 13:24:30 +0200

    CodeBlockRenderer -> refactor

--- src/Extensions/Spectre.Console.Extensions.Markup/Renderers/CodeBlockRenderer.cs
+++ src/Extensions/Spectre.Console.Extensions.Markup/Renderers/CodeBlockRenderer.cs
@@ -11,6 +11,37 @@ internal sealed class CodeBlockRenderer : IRenderer<CodeBlock>
 {
     public IRenderable Render(CodeBlock codeBlock)
     {
+        var code = GetLinesAsString(codeBlock);
+
+        Panel? panel = null;
+
+        if (codeBlock is FencedCodeBlock fencedCodeBlock && !string.IsNullOrEmpty(fencedCodeBlock.Info))
+        {
+            panel = fencedCodeBlock.Info!.ToLowerInvariant() switch
+            {
+                "json" => new Panel(new JsonText(code)),
+                "csharp" => new Panel(new CSharpText(code)),
+                "xml" => new Panel(new XmlText(code)),
+                _ => null
+            };
+
+            if (panel is not null)
+            {
+                panel.Header = new PanelHeader(fencedCodeBlock.Info.EscapeMarkup());
+            }
+        }
+
+        panel ??= new Panel(code.EscapeMarkup());
+
+        panel.BorderStyle = Color.Blue;
+        panel.Padding = new Padding(1, 0, 0, 0);
+        panel.Border = new LeftBoxBorder();
+
+        return panel;
+    }
+
+    private static string GetLinesAsString(CodeBlock codeBlock)
+    {
         var sb = new StringBuilder();
         var lines = codeBlock.Lines.Lines;
 
@@ -19,43 +50,6 @@ internal sealed class CodeBlockRenderer : IRenderer<CodeBlock>
             sb.AppendLine(line.ToString());
         }
 
-
-        Panel? panel = null;
-
-        if (codeBlock is FencedCodeBlock fencedCodeBlock && !string.IsNullOrEmpty(fencedCodeBlock.Info))
-        {
-            if (string.Equals(fencedCodeBlock.Info, "json", StringComparison.OrdinalIgnoreCase))
-            {
-                var jsonText = new JsonText(sb.ToString().Trim());
-                panel = new Panel(jsonText)
-                {
-                    Header = new PanelHeader(fencedCodeBlock.Info.EscapeMarkup())
-                };
-            }
-            else if (string.Equals(fencedCodeBlock.Info, "csharp", StringComparison.OrdinalIgnoreCase))
-            {
-                var csharpText = new CSharpText(sb.ToString().Trim());
-                panel = new Panel(csharpText)
-                {
-                    Header = new PanelHeader(fencedCodeBlock.Info.EscapeMarkup())
-                };
-            }
-            else if (string.Equals(fencedCodeBlock.Info, "xml", StringComparison.OrdinalIgnoreCase))
-            {
-                var xmlText = new XmlText(sb.ToString().Trim());
-                panel = new Panel(xmlText)
-                {
-                    Header = new PanelHeader(fencedCodeBlock.Info.EscapeMarkup())
-                };
-            }
-        }
-
-        panel ??= new Panel(sb.ToString().EscapeMarkup().Trim());
-
-        panel.BorderStyle = Color.Blue;
-        panel.Padding = new Padding(1, 0, 0, 0);
-        panel.Border = new LeftBorder();
-
-        return panel;
+        return sb.ToString().Trim();
     }
 }
