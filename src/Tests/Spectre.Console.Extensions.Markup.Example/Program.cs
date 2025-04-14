5a4c19359d3dc72df5f3c20e4a99ae73efc52815
Colin Wilmans <colin.wilmans@crwsolutions.nl>
2025-04-14 12:53:39 +0200

    Program: Update console encoding to UTF-8
    - Added `using System.Text;` to enable Encoding usage
    - Set console output and input encoding to UTF-8 for Unicode support

--- src/Tests/Spectre.Console.Extensions.Markup.Example/Program.cs
+++ src/Tests/Spectre.Console.Extensions.Markup.Example/Program.cs
@@ -1,9 +1,14 @@
+using System.Text;
+
 namespace Spectre.Console.Extensions.Markup.Example;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        System.Console.OutputEncoding = Encoding.UTF8;
+        System.Console.InputEncoding = Encoding.UTF8;
+
         var md = MarkupExample.GetSampleText();
         var markup = new MarkdownRenderable(md);
         AnsiConsole.Write(markup);
